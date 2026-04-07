dde561a2f8779775e1d119dfaaf5d0df6d0d5685
takemi853 <takemi@katolab.nitech.ac.jp>
2026-04-08 02:00:04 +0900

    fix: images.remotePatterns に更新

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/sw_route_app",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: "https", hostname: "image.tmdb.org" },
+    ],
+  },
 };
 
 export default nextConfig;
